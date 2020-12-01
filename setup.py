2159a334ba3b55a3577b0d4ec6935f52c3369103
endalk2 <eb808826@gmail.com>
2020-12-01 18:53:26 +0300

    bumped version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setup(
     name="render_pdf",
-    version="0.1.8",
+    version="0.2.2",
     url="https://github.com/endalk200/render_pdf",
 
     author="Endalkachew Biruk",
